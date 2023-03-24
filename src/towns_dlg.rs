6d8f1e0945fc74d9312b78c8a44ec256f5556646
Barugon <16503728+Barugon@users.noreply.github.com>
2023-03-24 00:50:37 -0700

    Improved display

--- src/towns_dlg.rs
+++ src/towns_dlg.rs
@@ -1,12 +1,13 @@
 use crate::{
   ethos::{Siege, CABALISTS, TOWNS, VIRTUES},
-  util::AppState,
+  util::{self, AppState},
 };
 use eframe::{
-  egui::{Context, Grid, Key, RichText, WidgetText, Window},
-  emath::Align2,
+  egui::{Context, Grid, Key, Layout, RichText, WidgetText, Window},
+  emath::{Align, Align2},
   epaint::Color32,
 };
+use std::mem;
 
 pub struct TownsDlg {
   state: AppState,
@@ -54,48 +55,55 @@ impl TownsDlg {
       .default_size([available.width(), 0.0])
       .resizable(false)
       .show(ctx, |ui| {
-        Grid::new("towns_grid").show(ui, |ui| {
-          // Header.
-          const HEADER_COLOR: Color32 = Color32::from_rgb(229, 187, 123);
-          ui.label(RichText::from("Town (Devotional)  ").color(HEADER_COLOR));
-          ui.label(RichText::from("Cabalist  ").color(HEADER_COLOR));
-          ui.label(RichText::from("Remaining Time  ").color(HEADER_COLOR));
-          ui.end_row();
+        Grid::new("towns_grid")
+          .min_col_width((ui.available_width() - ui.spacing().item_spacing.x * 2.0) / 3.0)
+          .show(ui, |ui| {
+            // Header.
+            const HEADER_COLOR: Color32 = Color32::from_rgb(229, 187, 123);
+            ui.label(RichText::from("Town (Devotional)  ").color(HEADER_COLOR));
+            ui.centered_and_justified(|ui| {
+              ui.label(RichText::from("Cabalist  ").color(HEADER_COLOR));
+            });
+            ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+              ui.label(RichText::from("Remaining Time  ").color(HEADER_COLOR));
+            });
+            ui.end_row();
 
-          for (town_index, info) in towns.iter().enumerate() {
-            if info.0 {
-              // Show the town with the active color.
-              let text = format!("{} ({:?})  ", TOWNS[town_index], VIRTUES[town_index]);
-              ui.label(RichText::from(text).color(Color32::from_rgb(154, 229, 255)));
+            for (town_index, info) in towns.iter().enumerate() {
+              if info.0 {
+                // Show the town with the active color.
+                let text = format!("{} ({:?})  ", TOWNS[town_index], VIRTUES[town_index]);
+                ui.label(RichText::from(text).color(Color32::from_rgb(154, 229, 255)));
 
-              // List all the cabalists that are currently sieging.
-              let mut first = true;
-              for (cabalist_index, sieging) in info.1.iter().enumerate() {
-                if *sieging {
-                  if first {
-                    first = false;
-                  } else {
-                    // Show an empty label for the town.
-                    ui.label(WidgetText::default());
+                // List all the cabalists that are currently sieging.
+                let mut first = true;
+                for (cabalist_index, sieging) in info.1.iter().enumerate() {
+                  if *sieging {
+                    if !mem::take(&mut first) {
+                      // Show an empty label for the town.
+                      ui.label(WidgetText::default());
+                    }
+
+                    ui.centered_and_justified(|ui| {
+                      ui.label(format!("{}  ", CABALISTS[cabalist_index]));
+                    });
+                    ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                      ui.label(util::get_countdown_text(
+                        Default::default(),
+                        sieges[cabalist_index].remain_secs(),
+                      ));
+                    });
+                    ui.end_row();
                   }
-
-                  ui.label(format!("{}  ", CABALISTS[cabalist_index]));
-                  ui.label(get_countdown_text(sieges[cabalist_index].remain_secs()));
-                  ui.end_row();
                 }
+              } else {
+                // Show the town with the inactive color.
+                let text = format!("{} ({:?})  ", TOWNS[town_index], VIRTUES[town_index]);
+                ui.label(RichText::from(text).color(Color32::from_rgb(102, 154, 180)));
+                ui.end_row();
               }
-            } else {
-              // Show the town with the inactive color.
-              let text = format!("{} ({:?})  ", TOWNS[town_index], VIRTUES[town_index]);
-              ui.label(RichText::from(text).color(Color32::from_rgb(102, 154, 180)));
-              ui.label("None");
-
-              // TODO: find next siege.
-
-              ui.end_row();
             }
-          }
-        });
+          });
         ui.separator();
         ui.horizontal(|ui| {
           if ui.button("Close").clicked() {
@@ -118,13 +126,4 @@ impl TownsDlg {
       self.close();
     }
   }
-}
-
-/// Get the remaining time in HH:MM:SS format.
-fn get_countdown_text(sec: i32) -> String {
-  let min = sec / 60;
-  let sec = sec % 60;
-  let hour = min / 60;
-  let min = min % 60;
-  return format!("{hour:02}:{min:02}:{sec:02}");
 }
