0a23a5ce0a6bf4944934e902d3b290fcda5b87f7
Barugon <16503728+Barugon@users.noreply.github.com>
2024-12-17 19:56:39 -0800

    Upgrade `embed-resource`

--- win_icon.rs
+++ win_icon.rs
@@ -1,3 +1,6 @@
+use embed_resource::CompilationResult;
+
 fn main() {
-  embed_resource::compile("res/win_icon.rc", embed_resource::NONE);
+  let result = embed_resource::compile("res/win_icon.rc", embed_resource::NONE);
+  assert!(result == CompilationResult::Ok || result == CompilationResult::NotWindows);
 }
