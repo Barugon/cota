7f9b5d39f990f86cde35142cbfb20b740e988290
Barugon <16503728+Barugon@users.noreply.github.com>
2025-04-05 14:04:06 -0700

    Implement `compare_ignore_case` function

--- src/util.rs
+++ src/util.rs
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize};
 use std::{
   borrow::Cow,
   cell::RefCell,
-  mem,
+  cmp, mem,
   ops::{Range, RangeInclusive},
   sync::{
     Arc,
@@ -228,6 +228,12 @@ impl Cancel {
   pub fn is_canceled(&self) -> bool {
     self.canceled.load(Ordering::Relaxed)
   }
+}
+
+pub fn compare_ignore_case(lhs: &str, rhs: &str) -> cmp::Ordering {
+  let lhi = lhs.chars().flat_map(|c| c.to_uppercase());
+  let rhi = rhs.chars().flat_map(|c| c.to_uppercase());
+  lhi.cmp(rhi)
 }
 
 fn find_ignore_case(text: &str, find: &str) -> Option<Range<usize>> {
